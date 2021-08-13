f36e32f8e4224326e3d864234c73289496cb660f
Leonardo Bergamini <leonardo.bergamini@hotmail.com.br>
2021-08-13 10:15:41 -0300

    configuração @AutoWired do RestTemplate

--- src/main/java/com/brq/internet/projetojspbase/ProjetoJspBaseApplication.java
+++ src/main/java/com/brq/internet/projetojspbase/ProjetoJspBaseApplication.java
@@ -4,21 +4,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @Configuration
-@ComponentScan(basePackages={"com.brq.internet.projetojspbase"})
+@ComponentScan(basePackages = { "com.brq.internet.projetojspbase" })
 public class ProjetoJspBaseApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoJspBaseApplication.class, args);
 	}
-	
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(ProjetoJspBaseApplication.class);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
 }
