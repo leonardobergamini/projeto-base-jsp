3b9804ef7455eb63a272f59cb312c1d78546bb58
Leonardo Bergamini <leonardo.bergamini@hotmail.com.br>
2021-08-12 22:32:58 -0300

    quickstarter projeto

---
+++ src/main/java/com/brq/internet/projetojspbase/ProjetoJspBaseApplication.java
@@ -0,0 +1,24 @@
+package com.brq.internet.projetojspbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@ComponentScan(basePackages={"com.brq.internet.projetojspbase"})
+public class ProjetoJspBaseApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJspBaseApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjetoJspBaseApplication.class);
+	}
+
+}
