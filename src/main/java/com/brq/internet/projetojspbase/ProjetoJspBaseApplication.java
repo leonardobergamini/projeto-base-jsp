9faf8727a040de215dceed23c1b60bc208a50bf0
Leonardo Bergamini <leonardo.bergamini@hotmail.com.br>
2021-08-22 22:01:51 -0300

    pré configuração WebTarget

--- src/main/java/com/brq/internet/projetojspbase/ProjetoJspBaseApplication.java
+++ src/main/java/com/brq/internet/projetojspbase/ProjetoJspBaseApplication.java
@@ -1,5 +1,8 @@
 package com.brq.internet.projetojspbase;
 
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -7,13 +10,14 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @Configuration
 @ComponentScan(basePackages = { "com.brq.internet.projetojspbase" })
 public class ProjetoJspBaseApplication extends SpringBootServletInitializer {
 
+	public final static String PATH_API_BASE = "<URL API>";
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoJspBaseApplication.class, args);
 	}
@@ -24,8 +28,8 @@ public class ProjetoJspBaseApplication extends SpringBootServletInitializer {
 	}
 
 	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
+	public WebTarget webTarget() {
+		return ClientBuilder.newClient().target(PATH_API_BASE);
 	}
 	
 }
